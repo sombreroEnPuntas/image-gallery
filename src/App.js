3a1a403b8af34126647ccefe5c2f3531f21f87f7
Mati <matiasfornes@gmail.com>
2019-01-21 21:23:02 +0100

    feat: remove react branding

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 class App extends Component {
   render() {
@@ -9,15 +9,15 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            <code>{"image-gallery"}</code> demo app
           </p>
           <a
             className="App-link"
-            href="https://reactjs.org"
+            href="https://github.com/sombreroEnPuntas/image-gallery"
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            {"Check the code on github!"}
           </a>
         </header>
       </div>
