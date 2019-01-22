d74b4de9e979328bf938ee392aa8ed2d836d969f
Mati <matiasfornes@gmail.com>
2019-01-22 18:50:50 +0100

    feat: added mock images

---
+++ src/mocks/pictures.js
@@ -0,0 +1,24 @@
+const mockPictures = [
+  {
+    alt: 'some description 0',
+    src: 'https://source.unsplash.com/random/150x150?sig=0',
+  },
+  {
+    alt: 'some description 1',
+    src: 'https://source.unsplash.com/random/150x150?sig=1',
+  },
+  {
+    alt: 'some description 2',
+    src: 'https://source.unsplash.com/random/150x150?sig=2',
+  },
+  {
+    alt: 'some description 3',
+    src: 'https://source.unsplash.com/random/150x150?sig=3',
+  },
+  {
+    alt: 'some description 4',
+    src: 'https://source.unsplash.com/random/150x150?sig=4',
+  },
+]
+
+export default mockPictures
