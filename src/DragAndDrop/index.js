98341c364dc03c5e5c746f46945339992054b169
Mati <matiasfornes@gmail.com>
2019-01-28 03:11:52 +0100

    fix: functional DragAndDrop component for simplicity

--- src/DragAndDrop/index.js
+++ src/DragAndDrop/index.js
@@ -1,51 +1,29 @@
-import React, { Component } from 'react'
+import React from 'react'
 import PropTypes from 'prop-types'
 
 // Library Components
 import ImageDropZone from 'react-image-dropzone'
 
-class DragAndDrop extends Component {
-  state = {
-    file: null,
-  }
+const DragAndDrop = ({ addPicture, removePicture, src }) => (
+  <ImageDropZone
+    anySize
+    height={150}
+    imageDefault={src}
+    imageDeleted={removePicture}
+    imagePicked={addPicture}
+    // showButton
+    // showDeleteButton
+    style={{ width: '150px', height: '150px' }}
+    width={150}
+  />
+)
 
-  static propTypes = {
-    src: PropTypes.string,
-  }
-
-  static defaultProps = {}
-
-  handleDrop = ({ file, image }) => {
-    if (!!image && !!file && file.type.includes('image/')) {
-      this.setState({
-        file,
-      })
-    }
-  }
-
-  handleRemove = () => {
-    this.setState({
-      file: null,
-    })
-  }
-
-  render() {
-    const { src } = this.props
-
-    return (
-      <ImageDropZone
-        anySize
-        height={150}
-        imageDefault={src}
-        imageDeleted={this.handleRemove}
-        imagePicked={this.handleDrop}
-        // showButton
-        // showDeleteButton
-        style={{ width: '150px', height: '150px' }}
-        width={150}
-      />
-    )
-  }
+DragAndDrop.propTypes = {
+  addPicture: PropTypes.func.isRequired,
+  removePicture: PropTypes.func.isRequired,
+  src: PropTypes.string,
 }
+
+DragAndDrop.defaultProps = {}
 
 export default DragAndDrop
--- src/DragAndDrop/index.test.js
+++ src/DragAndDrop/index.test.js
@@ -1,49 +1,27 @@
 import React from 'react'
-import { mount } from 'enzyme'
+import { shallow } from 'enzyme'
 
 import DragAndDrop from './'
 
 // Mocks
 import mockPictures from '../mocks/pictures'
-const legitImageFile = { type: 'image/png', legit: 'mock file' }
-const legitPDFFile = { type: 'application/pdf', legit: 'mock file' }
 
 describe('DragAndDrop', () => {
   it('renders mocks as expected for: Pictures!', () => {
-    const wrapper = mount(<DragAndDrop src={mockPictures[0].src} />)
+    const wrapper = shallow(
+      <DragAndDrop
+        addPicture={jest.fn()}
+        removePicture={jest.fn()}
+        src={mockPictures[0].src}
+      />
+    )
     expect(wrapper.debug()).toMatchSnapshot()
   })
 
   it('rendersempty', () => {
-    const wrapper = mount(<DragAndDrop />)
+    const wrapper = shallow(
+      <DragAndDrop addPicture={jest.fn()} removePicture={jest.fn()} />
+    )
     expect(wrapper.debug()).toMatchSnapshot()
-  })
-
-  describe('handles', () => {
-    const wrapper = mount(<DragAndDrop />)
-
-    it('handleDrop invalid', () => {
-      wrapper.instance().handleDrop({
-        image: 'blob:http://localhost:3000/some-other-hash',
-        file: legitPDFFile,
-      })
-
-      expect(wrapper.state()).toEqual({ file: null })
-    })
-
-    it('handleDrop valid', () => {
-      wrapper.instance().handleDrop({
-        image: 'blob:http://localhost:3000/some-hash',
-        file: legitImageFile,
-      })
-
-      expect(wrapper.state()).toEqual({ file: legitImageFile })
-    })
-
-    it('handleRemove', () => {
-      wrapper.instance().handleRemove()
-
-      expect(wrapper.state()).toEqual({ file: null })
-    })
   })
 })
